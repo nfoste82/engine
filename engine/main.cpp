cb2da8daa5602b32a86cea49cc621b86b81525e8
Nic Foster <nfoster@backflipstudios.com>
2017-02-08 22:55:06 -0700

    Initial Commit

---
+++ engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  engine
+//
+//  Created by James Foster on 2/8/17.
+//  Copyright © 2017 Nic Foster. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
